bec2562271c644d207e5bcf17c9689f42a02a841
saikat00007 <dev.shoukatulislam@gmail.com>
2022-10-15 11:11:39 +0600

    d

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 import Logo from '../../assets/logo.png';
+import Bars from '../../assets/bars.png';
+import { Link } from 'react-scroll';
 
 const Header = () => {
+    const mobile = window.innerWidth <= 768 ? true : false;
+    const [menuOpened, setMenuOPened] = useState(false);
     return (
         <div className='header'>
+            <img src={Logo} alt="" className='logo' />
+            {menuOpened === false && mobile === true ? (
+                <div
+                    style={{ backgroundColor: "var(--appColor)", padding: "0.5rem", borderRadius: "5px" }}
+                    onClick={() => setMenuOPened(true)}
+                >
+                    <img src={Bars} alt="" style={{ width: '1.5rem', height: '1.5rem' }} />
+                </div>
+            ) : (
+                <ul className='header-menu'>
+                    <li>
+                        <Link
+                            onClick={() => setMenuOPened(false)}
+                            to='home'
+                            activeClass="active"
+                            span={true}
+                            smooth={true}
+                        >
+                            Home
+                        </Link>
+                    </li>
+                    <li>
 
-            <img src={Logo} alt="" className='logo'/>
-            <ul className='header-menu'>
-                <li>Home</li>
-                <li>Programs</li>
-                <li>Why us</li>
-                <li>Plans</li>
-                <li>Testimonials</li>
-            </ul>
+                        <Link
+
+                            onClick={() => setMenuOPened(false)}
+                            to='programs'
+
+                            span={true}
+                            smooth={true}
+                        >Programs</Link></li>
+                    <li>
+                        <Link
+
+                            onClick={() => setMenuOPened(false)}
+                            to='Reasons'
+
+                            span={true}
+                            smooth={true}
+                        >Why us</Link></li>
+                    <li>
+                        <Link
+
+                            onClick={() => setMenuOPened(false)}
+                            to='plans'
+
+                            span={true}
+                            smooth={true}
+                        >Plans</Link></li>
+                    <li
+
+                    >
+                        <Link
+                            onClick={() => setMenuOPened(false)}
+                            to='Testimonials'
+
+                            span={true}
+                            smooth={true}
+                        >
+                            Testimonials
+                        </Link>
+                    </li>
+                </ul>
+            )}
         </div>
+
     );
 };
 
--- src/components/Plans/Plans.jsx
+++ src/components/Plans/Plans.jsx
@@ -4,7 +4,7 @@ import { plansData } from '../../data/plansData';
 import WhiteTick from '../../assets/whiteTick.png';
 const Plans = () => {
     return (
-        <div className='plans-container'>
+        <div className='plans-container' id='plans'>
             <div className="blur plans-blur-1"></div>
             <div className="blur plans-blur-2"></div>
             <div className="Programs-header" style={{ gap: '2rem' }}>
